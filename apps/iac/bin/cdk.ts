6d6e14ef3fb803d8963ad0e671416ad8d9556ef5
Chris Harper <chris.harper@gitkraken.com>
2022-12-07 16:05:22 -0600

    getting eks running on fargate

--- apps/iac/bin/cdk.ts
+++ apps/iac/bin/cdk.ts
@@ -2,5 +2,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 
+// const env = {
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   region: process.env.CDK_DEFAULT_REGION
+// };
+const env = {
+  account: '014278456228',
+  region: 'us-east-1',
+  stage: 'dev',
+};
+
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new CdkStack(app, `${env.stage}-test-quality-poc`, { env });
--- apps/iac/lib/cdk-stack.ts
+++ apps/iac/lib/cdk-stack.ts
@@ -1,19 +1,17 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { CdkEksFargateStack } from './stacks/cdk-eks-fargate-stack';
 
 export class CdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    new CdkEksFargateStack(scope, 'k8s-app-on-eks-fargate-stack', {
+      env: props.env,
+      clusterName: 'test-quaility-poc', // if you don't specify the EKS cluster name, CDK will create the name for you.
+      // vpcId: "vpc-f73f4e8a",
+      // If you speficy an existing vpc-id, then CDK will use this existing VPC to create EKS cluster instead of
+      // creating a new VPC. Please note when using an existing VPC, please make sure its private subnets have been tagged with
+      // "kubernetes.io/role/internal-elb: 1" as described in https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html
     });
-
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
